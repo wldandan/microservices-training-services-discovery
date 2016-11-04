56fc1b6a1c182117b66dbdf86a69cabcffd01fc4
Wang lei <wldandan@gmail.com>
2016-11-04 19:58:10 +0800

    update docker-compose

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+cd eureka-server && ./gradlew clean build && cd -
+cd service-registry/event-service && ./gradlew clean build && cd -
+cd service-registry/review-service && ./gradlew clean build && cd -
